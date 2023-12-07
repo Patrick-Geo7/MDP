6ef557a973a7c9b1f18ac048f4c671f0a499f693
mostafa336 <96492072+mostafa336@users.noreply.github.com>
2023-12-07 17:25:35 +0200

    added policy iteration

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 n_rows, n_cols = 3, 3
-actions = ["Up", "Down", "Right", "Left"]
+actions = ['Up', 'Down', 'Right', 'Left']
 
 p_correct = 0.8  # probability that the agent goes in the direction it selects
 p_wrong = 0.1  # probability it moves at right angles to the intended direction
@@ -10,13 +10,13 @@ gamma = 0.99  # Discount factor
 
 
 def get_new_state(i, j, action):
-    if action == "Up":
+    if action == 'Up':
         return max(i - 1, 0), j
-    elif action == "Down":
+    elif action == 'Down':
         return min(i + 1, n_rows - 1), j
-    elif action == "Right":
+    elif action == 'Right':
         return i, min(j + 1, n_cols - 1)
-    elif action == "Left":
+    elif action == 'Left':
         return i, max(j - 1, 0)
 
 
@@ -50,7 +50,7 @@ def value_iteration(r):
                     total_expected_reward += p_correct * (rewards[new_i][new_j] + gamma * V[new_i][new_j])
                     # Right angles to the intended direction
                     for side_action in (
-                    actions[(actions.index(action) + 1) % 4], actions[(actions.index(action) - 1) % 4]):
+                            actions[(actions.index(action) + 1) % 4], actions[(actions.index(action) - 1) % 4]):
                         new_i, new_j = get_new_state(i, j, side_action)
                         if i == new_i and j == new_j:
                             continue
@@ -94,12 +94,102 @@ def value_iteration(r):
     return V, policy
 
 
+r_values = [100, 3, 0, -3]
+
+def calculate_state_value(i, j, action, V, rewards):
+    # Calculate value for moving in intended direction
+    new_i, new_j = get_new_state(i, j, action)
+
+    value = p_correct * (rewards[new_i, new_j] + gamma * V[new_i, new_j])
+
+    if i == new_i and j == new_j:
+        value = 0
+    # Calculate value for moving at right angles
+    for side_action in [actions[(actions.index(action) - 1) % 4], actions[(actions.index(action) + 1) % 4]]:
+        side_i, side_j = get_new_state(i, j, side_action)
+        if i == side_i and j == side_j:
+            continue
+        value += p_wrong * (rewards[side_i, side_j] + gamma * V[side_i, side_j])
+
+    return value
+
+def policy_evaluation(policy, V, rewards):
+    epsilon = 1e-6
+    while True:
+        delta = 0
+        for i in range(n_rows):
+            for j in range(n_cols):
+                if policy[i][j] == 'Terminal':
+                    continue
+                v = V[i, j]
+
+                # Get the action suggested by the current policy
+                action = policy[i][j]
+                new_value = 0
+
+                new_value += calculate_state_value(i, j, action, V, rewards)
+
+                V[i][j] = new_value
+                delta = max(delta, abs(v - V[i][j]))
+
+        if delta < epsilon:
+            break
+    return V
+
+
+def policy_improvement(V, policy, rewards):
+    policy_stable = True
+    for i in range(n_rows):
+        for j in range(n_cols):
+            if policy[i][j] == 'Terminal':
+                continue
+            old_action = policy[i][j]
+            max_value = -np.inf
+            for action in actions:
+                action_value = calculate_state_value(i, j, action, V, rewards)
+                if action_value > max_value:
+                    max_value = action_value
+                    policy[i][j] = action
+            if old_action != policy[i][j]:
+                policy_stable = False
+    return policy, policy_stable
+
+
+def policy_iteration(r):
+    rewards = np.array([[r, -1, 10], [-1, -1, -1], [-1, -1, -1]])
+    V = np.zeros((n_rows, n_cols))
+    # Random initial policy
+    policy = np.empty((n_rows, n_cols), dtype='U10')
+    for i in range(n_rows):
+        for j in range(n_cols):
+            if (i == 0 and j == 0) or (i == 0 and j == 2):
+                policy[i][j] = 'Terminal'
+            else:
+                policy[i][j] = np.random.choice(actions)
+    policy[0][0], policy[0][2] = "Terminal", "Terminal"
+
+    while True:
+        V = policy_evaluation(policy, V, rewards)
+        policy, policy_stable = policy_improvement(V, policy, rewards)
+        if policy_stable:
+            break
+
+    return V, policy
+
 
 r_values = [100, 3, 0, -3]
 
 for r in r_values:
     V, policy = value_iteration(r)
+    print("//////////  Value     //////////////")
     print(f"Value function for r = {r}:")
     print(V)
     print(f"Policy for r = {r}:")
     print(policy)
+    print("//////////   POLICY  //////////\n")
+    V, policy = policy_iteration(r)
+    print(f"Value function for r = {r}:")
+    print(V)
+    print(f"Policy for r = {r}:")
+    print(policy)
+    print("////////////////////////////////////////////////////////////////////////////\n")
